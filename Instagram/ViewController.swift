41a8b9cd8b6cc551617328c435f49a209b96c62f
EfazAhmed <efazahmededu@gmail.com>
2021-08-03 15:49:32 -0400

    First commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Efaz on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
